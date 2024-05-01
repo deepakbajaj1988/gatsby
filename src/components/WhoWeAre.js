ac4517a08e8c8187bfae70dd2492d3df047bd3da
Deepak <deepak@rayanaweb.com>
2024-05-01 12:59:59 +0530

    update code

---
+++ src/components/WhoWeAre.js
@@ -0,0 +1,11 @@
+import * as React from "react"
+
+function AboutPage() {
+  return (
+    <div className="about-container">
+      <p className="text-[50px] bg-slate-100">About me.</p>  
+    </div>
+  )
+}
+
+export default AboutPage
